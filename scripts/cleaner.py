770c5f2f3e6fbc04747b0f8fdc2f38956631ffa2
Shrey <shrey.dabhi23@gmail.com>
2018-09-30 01:10:49 +0530

    Dataset cleaned & converted to raw text

--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -1,19 +1,31 @@
 from bs4 import BeautifulSoup as bs
-import re
+from os import walk
+import sys
 
-with open("../data/xml/aviators/Aaron_Buerge.xml") as f:
-    soup = bs(f.read(), "lxml")
-    init_data = str(soup("text")[0].string)
-    init_data = init_data.replace("''", "")
-    init_data = init_data.replace("'", "")
-    init_data = re.sub(r"{{.+?}}\n", "", init_data)
-    init_data = re.sub(r"{{[^}]+}}", "", init_data)
-    init_data = re.sub(r"{{[^}]+}}", "", init_data)
-    init_data = init_data.replace("[[", "")
-    init_data = init_data.replace("]]", "")
-    print(init_data)
-
-    # with open("trial.txt", "w+") as out:
-    #     out.write(init_data)
-    #     out.close()
-    f.close()
+init_path = "../data/xml/" + str(sys.argv[1]) + "/"
+dest_path = "../data/text/" + str(sys.argv[1]) + "/"
+with open("status_" + str(sys.argv[1]) + ".txt", "w", encoding="utf-8") as status:
+    for (dirpath, dirnames, filenames) in walk(init_path):
+        for data_file in filenames:
+            with open(init_path + data_file, encoding='utf-8') as f:
+                soup = bs(f.read(), "lxml")
+                init_data = str(soup("text")[0].string)
+                init_data = init_data.replace("''", "")
+                init_data = init_data.replace("'", "")
+                init_data = init_data.replace("|", " ")
+                init_data = init_data.replace("[[", "")
+                init_data = init_data.replace("]]", "")
+                data_list = init_data.split("\n")
+                try:
+                    end = data_list.index("==References==")
+                    start = data_list.index("}}")
+                    final_data = "\n".join(data_list[(start+1):end])
+                    final_data.strip()
+                    with open(dest_path + data_file[:-4] + ".txt", "w+", encoding='utf-8') as out:
+                        out.write(final_data)
+                        out.close()
+                    f.close()
+                    status.write(data_file + "\n")
+                except ValueError:
+                    status.write("empty: " + data_file + "\n")
+                    continue
